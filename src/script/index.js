32acbd9e3ba54a3877ef80e1c732ba86a478b08d
Aandi1989 <navaliny1989@gmail.com>
2023-04-09 20:43:26 +0300

    header media (migration of block to burgerMenu)

--- src/script/index.js
+++ src/script/index.js
@@ -4,9 +4,17 @@ userIcon.addEventListener('click', function(e){
     userMenu.classList.toggle('_active');
 })
 
+document.documentElement.addEventListener('click', function(e){
+    if(!e.target.closest('.userHeader')){
+        let userMenu = document.querySelector('.userHeader__menu');
+        userMenu.classList.remove('_active')
+    }
+});
+
 let iconMenu = document.querySelector('.iconMenu')
 let menuBody = document.querySelector('.menu__body')
 iconMenu.addEventListener('click', function(e){
     iconMenu.classList.toggle('_active');
     menuBody.classList.toggle('_active')
 })
+
