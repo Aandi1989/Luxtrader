b5801b0584697eb7a7d56acfcb9aa1567d8e988a
Aandi1989 <navaliny1989@gmail.com>
2023-04-10 21:04:12 +0300

    slider completed (without z-index)

--- src/script/index.js
+++ src/script/index.js
@@ -30,3 +30,48 @@ ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('
 }
 ibg();
 //-----------------
+
+//BildSlider
+let sliders = document.querySelectorAll('._swiper');
+console.log(sliders)
+if(sliders){
+    for(let index = 0; index <sliders.length; index++){
+        let slider = sliders[index];
+        if(!slider.classList.contains('swiper-bild')){
+            let slider_items = slider.children;
+            if(slider_items){
+                for(let index = 0; index < slider_items.length; index++){
+                    let el = slider_items[index];
+                    el.classList.add('swiper-slide');
+                }
+            }
+            let slider_content = slider.innerHTML;
+            let slider_wrapper = document.createElement('div');
+            slider_wrapper.classList.add('swiper-wrapper');
+            slider_wrapper.innerHTML = slider_content;
+            slider.innerHTML = '';
+            slider.appendChild(slider_wrapper);
+            slider.classList.add('swiper-bild');
+        }
+        if(slider.classList.contains('_gallery')){
+            //slider.data('lightGallery').destroy(true);
+        }
+    }
+    sliders_bild_callback();
+}
+
+function sliders_bild_callback(params){}
+
+let main_slider = new Swiper('.mainSlider__body',{
+    observer:true,
+    observeParents:true,
+    slidesPerView:1,
+    spaceBetween:0,
+    // autoHeight:true,
+    speed:800,
+    
+    navigation:{
+        nextEl:'.controlMainSlider__arrow_next',
+        prevEl:'.controlMainSlider__arrow_prev',
+    }
+})
