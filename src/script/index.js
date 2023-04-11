17057a1333fa712753e7e0d43d9a8b987614d212
Aandi1989 <navaliny1989@gmail.com>
2023-04-11 14:53:54 +0300

    Block lots before slider

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,9 @@
+// userIcon
 let userIcon = document.querySelector('.userHeader__icon')
 userIcon.addEventListener('click', function(e){
     let userMenu = document.querySelector('.userHeader__menu');
     userMenu.classList.toggle('_active');
+   
 })
 
 document.documentElement.addEventListener('click', function(e){
@@ -10,14 +12,17 @@ document.documentElement.addEventListener('click', function(e){
         userMenu.classList.remove('_active')
     }
 });
+// ------------------------
 
+// BurgerMenu icon
 let iconMenu = document.querySelector('.iconMenu')
 let menuBody = document.querySelector('.menu__body')
 iconMenu.addEventListener('click', function(e){
     iconMenu.classList.toggle('_active');
-    menuBody.classList.toggle('_active')
+    menuBody.classList.toggle('_active');
+    document.body.classList.toggle('_lock')
 })
-
+// ---------------------
 
 // ibg function
 function ibg(){
